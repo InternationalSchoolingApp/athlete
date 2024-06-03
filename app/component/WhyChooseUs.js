07ca95227f9f3869a373d5471c1f8f2ee6c3e90a
aazimsheikh9 <sheikhaazim13@gmail.com>
2024-06-03 14:48:13 +0530

    home page form design fixed and icon changed. Left with meta data and content

--- app/component/WhyChooseUs.js
+++ app/component/WhyChooseUs.js
@@ -6,35 +6,39 @@ import PPCForm from "./PPCForm";
 const content = [
   {
     svg: ImagePath.BOOK_ICON,
-    alt: "icon",
+    alt: "book-icon",
     heading: "Personalized Learning",
     para: "We understand that everyone learns differently. That's why we offer personalized learning plans tailored to your child's individual needs and goals.",
+    width: "w-6",
   },
   {
-    svg: ImagePath.BOOK_ICON,
-    alt: "icon",
+    svg: ImagePath.TEACHER_ICON,
+    alt: "teacher-icon",
     heading: "Best Teachers",
     para: "Our trained and certified teachers from 50+ countries have helped countless student athletes achieve their academic goals.",
+    width: "w-7",
   },
   {
-    svg: ImagePath.BOOK_ICON,
-    alt: "icon",
+    svg: ImagePath.CURRICULUM_ICON,
+    alt: "curriculum-icon",
     heading: "American Curriculum",
     para: "Our academic programs are designed to prepare your child for success in college and beyond. From math and science to humanities and social sciences, we've got your child covered.",
+    width: "w-7",
   },
   {
-    svg: ImagePath.BOOK_ICON,
-    alt: "icon",
+    svg: ImagePath.CLASS_ICON,
+    alt: "class-icon",
     heading: "Flexible Class Schedule",
     para: "With our online platform, you can study whenever and wherever you want. Whether you're on the road for a tournament or training at your local gym, you can always stay on track with your coursework.",
+    width: "w-7",
   },
 ];
 
 const WhyChooseUs = () => {
   return (
     <>
-      <div className="flex justify-between items-center flex-col lg:flex-row bg-gradient-to-r from-[#542848] via-[#0D073E] to-[#542848] lg:p-10 p-5 rounded-3xl mt-10 lg:gap-10 gap-5">
-        <div className="">
+      <div className="flex justify-between items-center flex-col lg:flex-row bg-gradient-to-r from-[#542848] via-[#0D073E] to-[#542848] rounded-3xl lg:p-10 p-0 mt-10 lg:gap-10 gap-5">
+        <div className="lg:p-0 p-5">
           <h3 className="font-extrabold text-2xl lg:text-4xl mb-8 text-white">
             Why Choose Us?
           </h3>
@@ -46,7 +50,7 @@ const WhyChooseUs = () => {
                   height={100}
                   src={item.svg}
                   alt={item.alt}
-                  className="w-5 mb-1"
+                  className={`mb-1 ${item.width}`}
                 />
                 <h5 className="font-bold mb-1">{item.heading}</h5>
                 <p className="font-medium">{item.para}</p>
--- app/contact/page.js
+++ app/contact/page.js
@@ -7,7 +7,7 @@ const contact = () => {
   return (
     <div className="r-w flex flex-col lg:flex-row justify-center items-center pt-10 gap-10 !mb-12">
       <div className="text-black-800 w-3/4">
-        <h1 className="text-2xl lg:text-6xl font-bold mb-6 text-center lg:text-left">
+        <h1 className="g-t bg-gradient-to-r from-[#001FCF] to-[#007EFF] text-2xl lg:text-6xl font-bold mb-6 text-center lg:text-left">
           TALK TO US
         </h1>
         <p className="text-black-600 text-sm text-justify mb-6">
